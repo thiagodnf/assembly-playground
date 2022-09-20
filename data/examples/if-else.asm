59e8fb166eb5d0ff6fd0ed40a06d629ed08da7d1
Thiago Ferreira <thiagodnf@users.noreply.github.com>
2022-09-20 16:19:15 -0400

    Update if-else.asm

--- data/examples/if-else.asm
+++ data/examples/if-else.asm
@@ -1,13 +1,13 @@
-;  int a = 10
-;  int b = 20;
+;  int a = 1
+;  int b = 5;
 ;  int c = 0;
 ;  if(a < b){
 ;    c = 1;
 ;  } else {
 ;    c = -1;
 ;  }
-MOV #10, R2 ; int a = 10
-MOV #20, R3 ; int b = 20
+MOV #1, R2 ; int a = 1
+MOV #5, R3 ; int b = 5
 MOV #0, R2  ; int c = 0
 
 CMP R3, R2
