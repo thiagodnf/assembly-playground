66666f8167f81b47e26a149e2814031404aef047
Thiago Ferreira <thiagodnf@gmail.com>
2022-10-06 11:51:49 -0400

    Update cpu.js

--- js/components/cpu.js
+++ js/components/cpu.js
@@ -6,35 +6,35 @@ class CPU {
 
     constructor() {
 
-        this.$table = $(".panel-cpu .card-body table");
-        this.$tbody = this.$table.find('tbody');
+        // this.$table = $(".panel-cpu .card-body table");
+        // this.$tbody = this.$table.find('tbody');
 
-        let value = "0";
+        // let value = "0";
 
-        let regiterIds = ["PC", "SR", "R2", "R3", "R4", "R5", "R6"];
+        // let regiterIds = ["PC", "SR", "R2", "R3", "R4", "R5", "R6"];
 
-        for (const registerId of regiterIds) {
+        // for (const registerId of regiterIds) {
 
-            if (registerId === "SR") {
-                this.$tbody.append(`
-                    <tr>
-                        <td rowspan="2" class="px-3">${registerId}</td>
-                        <td class="text-center small my-0 py-0">N</td>
-                        <td class="text-center small my-0 py-0">Z</td>
-                    </tr>
-                    <tr>
-                        <td class="text-center small my-0 py-0" id="${registerId}">0</td>
-                        <td class="text-center small my-0 py-0" id="${registerId}">0</td>
-                    </tr>
-                `);
-            } else {
-                this.$tbody.append(`
-                    <tr>
-                        <td class="px-3">${registerId}</td>
-                        <td class="px-3" id="${registerId}" colspan="2">0</td>
-                    </tr>
-                `);
-            }
-        }
+        //     if (registerId === "SR") {
+        //         this.$tbody.append(`
+        //             <tr>
+        //                 <td rowspan="2" class="px-3">${registerId}</td>
+        //                 <td class="text-center small my-0 py-0">N</td>
+        //                 <td class="text-center small my-0 py-0">Z</td>
+        //             </tr>
+        //             <tr>
+        //                 <td class="text-center small my-0 py-0" id="${registerId}">0</td>
+        //                 <td class="text-center small my-0 py-0" id="${registerId}">0</td>
+        //             </tr>
+        //         `);
+        //     } else {
+        //         this.$tbody.append(`
+        //             <tr>
+        //                 <td class="px-3">${registerId}</td>
+        //                 <td class="px-3" id="${registerId}" colspan="2">0</td>
+        //             </tr>
+        //         `);
+        //     }
+        // }
     }
 }
